ae6921a952c2c1d37bcee631cb2fd2da999804c1
Alyson Souza <alysonforever@gmail.com>
2022-03-02 18:24:16 -0300

    add React Router

---
+++ src/Pages/EmailList/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './EmailList.css'
+
+function EmailList() {
+  return (
+    <div className="email-list">
+      <h2>Email List</h2>
+    </div>
+  )
+}
+
+export default EmailList
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import './Home.css';
 import Header from '../../Components/Header';
 import Sidebar from '../../Components/Sidebar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Mail from '../Mail';
+import EmailList from '../EmailList';
 
-function App() {
+function App () {
   return (
-    <div className="app">
-      <Header />
-      <Sidebar />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Routes>
+            <Route path='/mail' element={<Mail />} />
+            <Route path='/' element={<EmailList />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
