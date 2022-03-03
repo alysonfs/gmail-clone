a2dc321cd491e37f0071551cc95461bc33523df8
Alyson Souza <alysonforever@gmail.com>
2022-03-03 11:01:55 -0300

    add mail page and navigate

--- src/Pages/Mail/index.js
+++ src/Pages/Mail/index.js
@@ -1,10 +1,52 @@
 import React from 'react'
 import './Mail.css'
 
-function Mail() {
+import {
+  ArrowBack, CheckCircle, Delete,
+  Email, Error, ExitToApp, LabelImportant, MoreVert,
+  MoveToInbox, Print, UnfoldMore, WatchLater
+} from '@mui/icons-material'
+import { IconButton } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
+
+function Mail () {
+  let navigate = useNavigate()
+
   return (
     <div className="mail">
-      <h1>MAIL</h1>
+      <div className="mail__tools">
+        <div className="mail__tools-left">
+          <IconButton
+            onClick={() => navigate('/')}>
+            <ArrowBack />
+          </IconButton>
+          <IconButton><MoveToInbox /></IconButton>
+          <IconButton><Error /></IconButton>
+          <IconButton><Delete /></IconButton>
+          <IconButton><Email /></IconButton>
+          <IconButton><WatchLater /></IconButton>
+          <IconButton><CheckCircle /></IconButton>
+          <IconButton><LabelImportant /></IconButton>
+          <IconButton><MoreVert /></IconButton>
+        </div>
+        <div className="mail__tools-right">
+          <IconButton><UnfoldMore /></IconButton>
+          <IconButton><Print /></IconButton>
+          <IconButton><ExitToApp /></IconButton>
+        </div>
+      </div>
+      <div className="mail__body">
+        <div className="mail__body-header">
+          <h2>Subject</h2>
+          <LabelImportant className='mail__body-icon-important'/>
+          <p>Title</p>
+          <p className='mail__body-time'>10pm</p>
+        </div>
+        <div className="mail__body-message">
+          <p>This message</p>
+        </div>
+      </div>
     </div>
   )
 }
